933206bb3e5b40ac6ff5dcc2733d5c0225fb9bd8
lexteer <penealeksander@gmail.com>
2026-09-12 14:16:16 +0200

    vector math

---
+++ src/main/java/physi2d/math/MathUtils.java
@@ -0,0 +1,7 @@
+package physi2d.math;
+
+public final class MathUtils {
+    public static final double EPSILON = 1e-9;
+
+    private MathUtils() {}
+}
---
+++ src/main/java/physi2d/math/Vec2.java
@@ -0,0 +1,44 @@
+package physi2d.math;
+
+public record Vec2(double x, double y) {
+    public static final Vec2 ZERO = new Vec2(0,0);
+
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "]";
+    }
+
+    public Vec2 add(Vec2 other) {
+        return new Vec2(this.x + other.x, this.y + other.y);
+    }
+
+    public Vec2 sub(Vec2 other) {
+        return new Vec2(this.x - other.x, this.y - other.y);
+    }
+
+    public Vec2 mult(double scalar) {
+        return new Vec2(this.x * scalar, this.y * scalar);
+    }
+
+    public Vec2 negate() {
+        return mult(-1);
+    }
+
+    public double dot(Vec2 other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    public double lengthSquared() {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vec2 normalize() {
+        double len = length();
+        if (len < MathUtils.EPSILON) throw new ArithmeticException("Cannot normalize a vector with a length of 0");
+        return new Vec2(this.x / len, this.y / len);
+    }
+}
